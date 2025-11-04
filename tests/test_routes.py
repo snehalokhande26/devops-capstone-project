0bac16faeca69deede944049c55a09b380cb73de
snehalokhande26 <imsnehalokhande@gmail.com>
2025-11-04 11:16:08 -0500

    Added CD pipeline updates

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -173,7 +173,7 @@ class TestAccountService(TestCase):
 
     def test_method_not_allowed(self):
         """It should not allow an illegal method call"""
-        resp=self.client.delete(BASE_URL)
+        resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def test_security_headers(self):
